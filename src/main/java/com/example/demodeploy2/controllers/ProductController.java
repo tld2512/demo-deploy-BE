e89f64d9e35f11f07be59fa5140be57c3164fcb2
tld2512 <tienlongdinh251297@gmail.com>
2019-12-13 17:27:23 +0700

    edit placing

--- src/main/java/com/example/demodeploy2/controllers/ProductController.java
+++ src/main/java/com/example/demodeploy2/controllers/ProductController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
 @CrossOrigin("*")
+@RestController
 public class ProductController {
     @Autowired
     private IProductService productService;
