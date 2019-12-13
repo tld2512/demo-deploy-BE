c2517b33090ea3fc81566b24d6939e32efc26d30
tld2512 <tienlongdinh251297@gmail.com>
2019-12-13 16:23:32 +0700

    demo deploy v2

---
+++ src/main/java/com/example/demodeploy2/DemoDeploy2Application.java
@@ -0,0 +1,13 @@
+package com.example.demodeploy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDeploy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDeploy2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demodeploy2/DemoDeploy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodeploy2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDeploy2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
